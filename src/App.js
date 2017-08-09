3fd814f39d05a5b849114727514005c7e02c461c
vattytrivedi <trivedivatsal005@gmail.com>
2017-08-09 22:26:44 +0530

    initial: App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ class App extends Component {
     render() {
         return (
             <div>
-            <h3> Hello World</h3>
-        </div>
+                //TODO - Adding a welcome page
+                <h3>Welcome</h3>
+            </div>
         );
     }
 }
